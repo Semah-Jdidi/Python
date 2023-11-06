925903c134080bc9c1f3b736db19611a603f98a7
Semah-Jdidi <semahjdidi73@gmail.com>
2023-11-06 09:29:26 +0100

    Update User.py

--- fundamentals/oop/practice/User.py
+++ fundamentals/oop/practice/User.py
@@ -5,35 +5,23 @@ class user:
         self.account_balance = 0
     
     def deposit(self, amount):
-        self.account_balance += amount  
+        self.account_balance += amount
+        return self
     
     def withdraw(self, amount):
         self.account_balance -= amount
+        return self
     
     def display_user_balance(self):
         print(f'User name: {self.name} \n' f'user balance: {self.account_balance}')
+        return self
 
 user_1 = user("John","John@gmail.com")
 user_2 = user("Adrien","John@gmail.com")
 user_3 = user("Bob","John@gmail.com")
 
-user_1.deposit(200)
-user_1.deposit(300)
-user_1.deposit(500)
-user_1.withdraw(400)
+user_1.deposit(200).deposit(300).deposit(500).withdraw(400).display_user_balance()
 
-user_1.display_user_balance()
+user_2.deposit(200).deposit(200).withdraw(100).withdraw(50).display_user_balance()
 
-user_2.deposit(200)
-user_2.deposit(200)
-user_2.withdraw(100)
-user_2.withdraw(50)
-
-user_2.display_user_balance()
-
-user_3.deposit(2000)
-user_3.withdraw(250)
-user_3.withdraw(150)
-user_3.withdraw(200)
-
-user_3.display_user_balance()
+user_3.deposit(2000).withdraw(250).withdraw(150).withdraw(200).display_user_balance()
